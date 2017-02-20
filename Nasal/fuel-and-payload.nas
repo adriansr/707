adriansr/707

commit f3b84648d53e4312287353b029aa953cd1106e69

707: fuel-and-payload.nas: bug fix: engines 1 and 4 continue to burn fuel while transferring fuel from reserve tanks to main tanks.
git-svn-id: http://svn.code.sf.net/p/flightgear/fgaddon/trunk/Aircraft/707@2430 9eb9bc84-db9d-4e05-893b-26dd41ac94a6

Nasal/fuel-and-payload.nas
@@ -1015,7 +1015,8 @@ var crossfeed_action = maketimer (4.0, func {
 		}else{
 				var mNeu = tfM1.getValue() + tfR1.getValue();
 	    	var rNeu = 0;
-		}	
+		}
+                mNeu -= getprop("/engines/engine[1]/fuel-flow_pph")/3600*4;
 		interpolate("/consumables/fuel/tank[6]/level-lbs", rNeu, 4);
 	  	interpolate("/consumables/fuel/tank[5]/level-lbs", mNeu, 4);
                 R1_to_M1 = 1;
@@ -1034,7 +1035,8 @@ var crossfeed_action = maketimer (4.0, func {
 		}else{
 				var mNeu = tfM4.getValue() + tfR4.getValue();
 	    	var rNeu = 0;
-		}	
+		}
+                mNeu -= getprop("/engines/engine[4]/fuel-flow_pph")/3600*4;
 		interpolate("/consumables/fuel/tank[0]/level-lbs", rNeu, 4);
 	    interpolate("/consumables/fuel/tank[1]/level-lbs", mNeu, 4);
             R4_to_M4 = 1;

commit 3e90be0214abb649d72d236533b5e69bb4866d7a

707: fuel-and-payload.nas: fix an off-by-one error introduced in r2430.
git-svn-id: http://svn.code.sf.net/p/flightgear/fgaddon/trunk/Aircraft/707@2440 9eb9bc84-db9d-4e05-893b-26dd41ac94a6

Nasal/fuel-and-payload.nas
@@ -1016,7 +1016,7 @@ var crossfeed_action = maketimer (4.0, func {
 				var mNeu = tfM1.getValue() + tfR1.getValue();
 	    	var rNeu = 0;
 		}
-                mNeu -= getprop("/engines/engine[1]/fuel-flow_pph")/3600*4;
+                mNeu -= getprop("/engines/engine[0]/fuel-flow_pph")/3600*4;
 		interpolate("/consumables/fuel/tank[6]/level-lbs", rNeu, 4);
 	  	interpolate("/consumables/fuel/tank[5]/level-lbs", mNeu, 4);
                 R1_to_M1 = 1;
@@ -1036,7 +1036,7 @@ var crossfeed_action = maketimer (4.0, func {
 				var mNeu = tfM4.getValue() + tfR4.getValue();
 	    	var rNeu = 0;
 		}
-                mNeu -= getprop("/engines/engine[4]/fuel-flow_pph")/3600*4;
+                mNeu -= getprop("/engines/engine[3]/fuel-flow_pph")/3600*4;
 		interpolate("/consumables/fuel/tank[0]/level-lbs", rNeu, 4);
 	    interpolate("/consumables/fuel/tank[1]/level-lbs", mNeu, 4);
             R4_to_M4 = 1;